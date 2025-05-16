07c829f5f401b223a6af838d0bf9b3269877c928
Máté Harrer <harrer.mate@petrik.hu>
2025-05-16 11:20:32 +0200

    Kerdes osztaly kesz

--- legyenOnIsMilliomos/Kerdes.cs
+++ legyenOnIsMilliomos/Kerdes.cs
@@ -8,7 +8,20 @@ namespace legyenOnIsMilliomos
 {
     internal class Kerdes
     {
+        public string Nehezseg { get; set; }
+        public string KerdesSzoveg { get; set; }
+        public List<string> Valaszok { get; set; }
+        public string HelyesValasz { get; set; }
+        public string Kategoria { get; set; }
 
+        public Kerdes(string nehezseg, string kerdesSzoveg, List<string> valaszok, string helyesValasz, string kategoria)
+        {
+            Nehezseg = nehezseg;
+            KerdesSzoveg = kerdesSzoveg;
+            Valaszok = valaszok;
+            HelyesValasz = helyesValasz;
+            Kategoria = kategoria;
+        }
 
     }
 }
