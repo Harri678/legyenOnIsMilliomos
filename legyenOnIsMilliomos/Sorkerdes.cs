7ef094a17ed0bb52de5cabccfb0a717db34e0b15
Máté Harrer <harrer.mate@petrik.hu>
2025-05-16 11:20:10 +0200

    Sorkerdes osztaly kesz

---
+++ legyenOnIsMilliomos/Sorkerdes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace legyenOnIsMilliomos
+{
+    internal class Sorkerdes
+    {
+        public string Kerdes { get; set; }
+        public List<string> Valaszok { get; set; }
+        public string HelyesSorrend { get; set; }
+        public string Kategoria { get; set; }
+
+        public Sorkerdes(string kerdes, List<string> valaszok, string helyesSorrend, string kategoria)
+        {
+            Kerdes = kerdes;
+            Valaszok = valaszok;
+            HelyesSorrend = helyesSorrend;
+            Kategoria = kategoria;
+        }
+    }
+}
